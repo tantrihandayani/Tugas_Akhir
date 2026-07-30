90753d8c601d1fba95dce45f75f1326b648473f6
tantrihandayani <tantriyandayani@gmail.com>
2026-07-30 11:38:37 +0700

    push akhir

--- app/(publlic)/login/page.tsx
+++ app/(publlic)/login/page.tsx
@@ -39,7 +39,7 @@ export default function CustomerLogin() {
 
         alert("Login berhasil");
 
-        if (data.is_staff === true) {
+        if (data.role === "admin") {
           router.push("/admin/adminHome");
         } else {
           router.push("/");
